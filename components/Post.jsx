f09908f1fabafa6ac41bb4e35dc87ea4784db3ff
Chris Mittell <mittell@hotmail.co.uk>
2022-09-02 09:03:38 +0900

    chore: added eslint comments

--- components/Post.jsx
+++ components/Post.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 const Post = ({ post }) => {
 	return (
 		<div className='card'>
+			{/* eslint-disable-next-line @next/next/no-img-element */}
 			<img src={post.frontmatter.cover_image} alt='' />
 			<div className='post-date'>Posted on {post.frontmatter.date}</div>
 			<h3>{post.frontmatter.title}</h3>
--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -17,6 +17,7 @@ const PostPage = ({
 			<div className='card card-page'>
 				<h1 className='post-title'>{title}</h1>
 				<div className='post-date'>Posted on {date}</div>
+				{/* eslint-disable-next-line @next/next/no-img-element */}
 				<img src={cover_image} alt='' />
 				<div className='post-body'>
 					<div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
